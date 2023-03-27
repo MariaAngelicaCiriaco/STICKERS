2ba070b0ef7a8a7674af72dd2c80b575ab8a3401
Maria Angelica Ciriaco <115666551+MariaAngelicaCiriaco@users.noreply.github.com>
2023-03-27 17:04:38 -0300

    finalizando a primeira aula

--- STICKERS/src/App.java
+++ STICKERS/src/App.java
@@ -3,6 +3,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -19,10 +21,18 @@ public class App {
        //System.out.println(body);
 
        //segundo passo: extrair os dados que interessam da lista
+       var parser = new JsonParser();
+       List<Map<String, String>> listaDeFilmes = parser.parse(body);
        
+       //System.out.println(listaDeFilmes.size());
 
-
-
+       //exibir e manipular os dados
+       for (Map<String,String> map : listaDeFilmes) {
+        System.out.println(map.get("title"));
+        System.out.println(map.get("image"));
+        System.out.println(map.get("imDbRating"));
+        
+       }
       
     }
 }
